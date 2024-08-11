4eb3c2b793bb30287a396480c3f7c88eaee305e9
esangayt <24_de_7@protonmail.com>
2024-08-11 17:04:17 -0500

    Diseño SideMenu

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
+import {SidemenuComponent} from "../shared/sidemenu/sidemenu.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet, SidemenuComponent
   ],
   templateUrl: './dashboard.component.html',
   styles: ``
