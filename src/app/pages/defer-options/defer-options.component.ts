8a3237801bba2bdcc7a7204f8c0c8373effcc3f3
esangayt <24_de_7@protonmail.com>
2024-08-12 07:29:25 -0500

    Defer Interacciones

--- src/app/pages/defer-options/defer-options.component.ts
+++ src/app/pages/defer-options/defer-options.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {HeavyLoadersFastComponent} from "@shared/heavyLoaders/heavy-loaders-fast/heavy-loaders-fast.component";
+import {TitleComponent} from "@shared/title/title.component";
 
 @Component({
   selector: 'app-defer-options',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeavyLoadersFastComponent, TitleComponent],
   templateUrl: './defer-options.component.html',
   styleUrl: './defer-options.component.css'
 })
 export class DeferOptionsComponent {
 
+  constructor() {
+    console.log(' Fast creado');
+  }
 }
--- src/app/shared/heavyLoaders/heavy-loaders-fast/heavy-loaders-fast.component.ts
+++ src/app/shared/heavyLoaders/heavy-loaders-fast/heavy-loaders-fast.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-heavy-loaders-fast',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './heavy-loaders-fast.component.html',
   styleUrl: './heavy-loaders-fast.component.css'
 })
 export class HeavyLoadersFastComponent {
+  @Input({ required: true }) cssClass!: string;
 
   constructor() {
     console.log('HeavyLoadersFastComponent created');
