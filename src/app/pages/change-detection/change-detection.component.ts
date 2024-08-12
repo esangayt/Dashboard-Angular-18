7f6421af9097dd26fa0cde7f855e52eb4f73637a
esangayt <24_de_7@protonmail.com>
2024-08-11 22:46:42 -0500

    Prueba ZoneLess

--- src/app/pages/change-detection/change-detection.component.ts
+++ src/app/pages/change-detection/change-detection.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, signal} from '@angular/core';
+import {TitleComponent} from "@shared/title/title.component";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TitleComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './change-detection.component.html',
   styles: ``
 })
 export class ChangeDetectionComponent {
+  public currentFramework = computed(() =>
+    `Current framework is ${this.frameworkAsSignal().name} ${this.frameworkAsSignal().version}`
+  )
 
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    version: '12.0.0'
+  })
+
+  public frameworkAsProperty = signal({
+    name: 'Angular',
+    version: '14.0.0'
+  })
+
+  constructor() {
+    setTimeout(() => {
+      this.frameworkAsSignal.update(frame => ({
+        ...frame, version: '13.0.0'
+      }))
+      console.log('Signal updated')
+    }, 3000);
+
+  }
 }
