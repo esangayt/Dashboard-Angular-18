1b9657ff375676364f1582c02f180cfa135b6023
esangayt <24_de_7@protonmail.com>
2024-08-15 15:01:53 -0500

    Angular Material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,13 @@ import {provideRouter, withViewTransitions} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection(
       {eventCoalescing: true}
     ), provideRouter(routes, withViewTransitions()),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()), provideAnimationsAsync()
   ]
 };
